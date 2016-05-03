18e4db08a0ef472556c5c334491cf7ebc2863900
owen <owen.ballentine@e-solutions.uk.com>
2016-05-03 09:38:56 +0100

    added WifiFallBack method

--- init.lua
+++ init.lua
@@ -1,22 +1,24 @@
---Using integer firmware multiplying RGB by 4 is as good as setting the PWM as a percentage of RGB -- EXAMPLE -- pwmValue = (rgbValue / 255) * 1023
+--Using integer firmware multiplying RGB by 4 is as good as setting the PWM as a percentage of RGB 
+--Percentage approach: pwmValue = (rgbValue / 255) * 1023
 pwmConversionFactor = 4;
 
---Set default colour to 
+--Set default colour
 defaultRGB = {}
 defaultRGB.red = 179;
 defaultRGB.green = 0;
 defaultRGB.blue = 179;
 
---Setup the Wifi
-dofile("wifi.lua")
-
 --pull in RGB functions
 dofile("rgb.lua")
-    --Initialise PWN with LED's off at 500hz
-    initRGB(redPin, greenPin, bluePin, 500, 0)
+
+--Initialise PWN with LED's off at 500hz
+initRGB(redPin, greenPin, bluePin, 500, 0);
     
-    --Turn LEDs on to default colour
-    setRGB(redPin, defaultRGB.red * pwmConversionFactor, greenPin, defaultRGB.green * pwmConversionFactor, bluePin, defaultRGB.blue * pwmConversionFactor)
+--Turn LEDs on to default colour
+setRGB(redPin, defaultRGB.red * pwmConversionFactor, greenPin, defaultRGB.green * pwmConversionFactor, bluePin, defaultRGB.blue * pwmConversionFactor);
 
 --set up the webpage
-dofile("colourPickerWeb.lua")
+dofile("colourPickerWeb.lua");
+
+--Setup the Wifi - could be delayed up to 10 seconds if has to fall back to AP mode
+dofile("wifi.lua");
--- wifi.lua
+++ wifi.lua
@@ -1,14 +1,24 @@
--- Wifi functions
-wifi.setmode(wifi.STATIONAP)
-
 --ESP as Wifi CLIENT settings
-ssid = "SSID"
-password = "PASSWORD"
-wifi.sta.config(ssid, password)
-wifi.sta.connect()
+wifi.setmode(wifi.STATION);
+dofile(wifi-credentials.lua);
+  --wifi-credentials.lua populates the ssid and password variables, example contents below:
+  --ssid = "Network Name";
+  --password = "Network Password";
+wifi.sta.config(ssid, password);
+wifi.sta.connect();
 
---ESP as WifiAP settings
-cfg={}
-cfg.ssid="RGB-Controller"
-cfg.pwd="LetThereBeLight"
-wifi.ap.config(cfg)
+function startWiFiAP()
+  --ESP as WifiAP settings
+  wifi.setmode(wifi.SOFTAP);
+  cfg={};
+  cfg.ssid="RGB-Controller";
+  cfg.pwd="LetThereBeLight";
+  wifi.ap.config(cfg);
+  end
+
+function wifiFallback()
+  --Method to determine if connected to a network, if not fall back to AP mode - More stable than STATIONAP method
+  if wifi.sta.status() ~= 5 then startWiFiAP() end
+  end
+
+tmr.alarm(0,10000,0,wifiFallback());
